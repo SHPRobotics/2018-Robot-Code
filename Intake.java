6726c791aa3b3394ed829045479c553419a97a4e
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2018-01-29 17:32:19 -0500

    Update Intake.java

--- Intake.java
+++ Intake.java
@@ -1,1 +1,31 @@
+package org.usfirst.frc.team5992.robot;
+import edu.wpi.first.wpilibj.*;
+
+public class Intake {
+
+	Victor in;
+	XboxController xbox;
+	
+	Intake (Victor intaker, XboxController xboxController) {
+		
+		in = intaker;
+		xbox = xboxController;
+	}
+	
+	public void takeCube () {
+		
+		if (xbox.getXButton()) {
+			
+			in.setSpeed(.5);
+			
+		}
+		
+		else {
+			
+			in.setSpeed(0);
+			
+		}
+		
+	}
+}
 
