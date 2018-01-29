e789edc55fd2c17b19eb1d6ef63395a05f53b13d
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2018-01-29 17:31:36 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,1 +1,35 @@
+package org.usfirst.frc.team5992.robot;
+import edu.wpi.first.wpilibj.*;
+
+public class Elevator {
+
+	DigitalInput div;
+	XboxController xbox;
+	Victor elevate;
+	
+	Elevator(DigitalInput digitalInputValue, XboxController xboxController, Victor elevator) {
+		
+		div = digitalInputValue;
+		xbox = xboxController;
+		elevate = elevator;
+		
+	}
+	
+	public void moveElevator() {
+		
+		if (div.get()) {
+			
+			elevate.setSpeed(xbox.getY(GenericHID.Hand.kRight));
+			
+		}
+		
+		else {
+			
+			elevate.setSpeed(0);
+			
+		}
+		
+	}
+	
+}
 
