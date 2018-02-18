195ee8bf08d12c7ef148ca790e8bd2679caeda76
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2018-02-18 16:45:26 -0500

    Update Drive.java

--- Drive.java
+++ Drive.java
@@ -6,6 +6,7 @@ public class Drive {
 
 	Joystick lj;
 	Joystick rj;
+	XboxController xbox;
 	Spark ld;
 	Spark rd;
 	
@@ -27,10 +28,129 @@ public class Drive {
 	
 	public void exponentialDrive() {
 		
-		ld.setSpeed(-((.7*Math.pow(lj.getY(), 3))+(.1*Math.pow(lj.getY(), 2))+(.2*lj.getY())));
-		rd.setSpeed(((.7*Math.pow(rj.getY(), 3))+(.1*Math.pow(rj.getY(), 2))+(.2*rj.getY())));
+		ld.setSpeed(-((.7*Math.pow(lj.getY(), 3))+(.3*lj.getY())));
+		rd.setSpeed(((.7*Math.pow(rj.getY(), 3))+(.3*rj.getY())));
 		
 	}
 	
+	public void linearXboxDrive() {
+		
+		if(xbox.getX(GenericHID.Hand.kLeft) > 0) {
+			
+			//Fowards Turning
+			ld.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			rd.setSpeed(((xbox.getTriggerAxis(GenericHID.Hand.kLeft))*(-Math.abs(xbox.getX(GenericHID.Hand.kLeft))+1)));
+			
+			//Backwards Turning
+			ld.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			rd.setSpeed(-((xbox.getTriggerAxis(GenericHID.Hand.kLeft))*(-Math.abs(xbox.getX(GenericHID.Hand.kLeft))+1)));
+			
+		}
+		
+		else if(xbox.getX(GenericHID.Hand.kLeft) < 0) {
+			
+			//Fowards Turning
+			rd.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			ld.setSpeed(-((xbox.getTriggerAxis(GenericHID.Hand.kLeft))*(-Math.abs(xbox.getX(GenericHID.Hand.kLeft))+1)));
+			
+			//Backwards Turning
+			rd.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			ld.setSpeed(((xbox.getTriggerAxis(GenericHID.Hand.kLeft))*(-Math.abs(xbox.getX(GenericHID.Hand.kLeft))+1)));
+			
+		}
+		
+		else if((xbox.getX(GenericHID.Hand.kLeft) == 1) && (xbox.getTriggerAxis(GenericHID.Hand.kRight) == 1)) {
+			
+			ld.setSpeed(-.5);
+			rd.setSpeed(-.5);
+			
+		}
+		
+		else if((xbox.getX(GenericHID.Hand.kLeft) == -1) && (xbox.getTriggerAxis(GenericHID.Hand.kRight) == 1)) {
+			
+			ld.setSpeed(.5);
+			rd.setSpeed(.5);
+			
+		}
+		
+		else {
+			
+			ld.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			rd.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			
+			ld.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			rd.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			
+		}
+		
+	}
+	
+	public void exponentialXboxDrive() {		
+		
+		if(xbox.getX(GenericHID.Hand.kLeft) > 0) {
+			
+			ld.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			rd.setSpeed((xbox.getTriggerAxis(GenericHID.Hand.kRight))*(-((.7*Math.abs(xbox.getX(GenericHID.Hand.kLeft))+(.3*(xbox.getX(GenericHID.Hand.kLeft)))))+1));
+			
+			ld.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			rd.setSpeed(-(xbox.getTriggerAxis(GenericHID.Hand.kRight))*(-((.7*Math.abs(xbox.getX(GenericHID.Hand.kLeft))+(.3*(xbox.getX(GenericHID.Hand.kLeft)))))+1));
+			
+		}
+		
+		else if(xbox.getX(GenericHID.Hand.kLeft) < 0) {
+			
+			rd.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			ld.setSpeed(-(xbox.getTriggerAxis(GenericHID.Hand.kRight))*(-((.7*Math.abs(xbox.getX(GenericHID.Hand.kLeft))+(.3*(xbox.getX(GenericHID.Hand.kLeft)))))+1));
+			
+			rd.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			ld.setSpeed((xbox.getTriggerAxis(GenericHID.Hand.kRight))*(-((.7*Math.abs(xbox.getX(GenericHID.Hand.kLeft))+(.3*(xbox.getX(GenericHID.Hand.kLeft)))))+1));
+			
+		}
+		
+		else if((xbox.getX(GenericHID.Hand.kLeft) == 1) && (xbox.getTriggerAxis(GenericHID.Hand.kRight) == 1)) {
+			
+			ld.setSpeed(-.5);
+			rd.setSpeed(-.5);
+			
+		}
+		
+		else if((xbox.getX(GenericHID.Hand.kLeft) == -1) && (xbox.getTriggerAxis(GenericHID.Hand.kRight) == 1)) {
+			
+			ld.setSpeed(.5);
+			rd.setSpeed(.5);
+			
+		}
+		
+		else {
+			
+			ld.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			rd.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kRight));
+			
+			ld.setSpeed(xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			rd.setSpeed(-xbox.getTriggerAxis(GenericHID.Hand.kLeft));
+			
+		}
+		
+	}
+	
+	/*public void seanTrapp() {
+		
+		final String David = "im depressed";
+		
+		if(xbox.getStartButton() && xbox.getAButton() && xbox.getTriggerAxis(GenericHID.Hand.kRight) == 1 && xbox.getBumper(GenericHID.Hand.kRight)) {
+			while(David == "im depressed") {
+					
+				System.out.println("David is depressed and will now proceed to kill himself");
+				
+			}
+		}	
+		
+		else {
+			
+			System.out.println("David is depressed but he won't kill himself");
+			
+		}
+		
+	}*/
+	
 }
-
