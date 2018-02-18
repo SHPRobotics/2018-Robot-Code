c551861a4b17ec1c0590ac4576f5bea1554e891a
Seton Hall Prep Robotics <setonhallrobotics@gmail.com>
2018-02-18 16:47:05 -0500

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -3,33 +3,93 @@ import edu.wpi.first.wpilibj.*;
 
 public class Elevator {
 
-	DigitalInput div;
+	DigitalInput limUp;
+	DigitalInput limDown;
 	XboxController xbox;
 	Victor elevate;
+	boolean override = true;
 	
-	Elevator(DigitalInput digitalInputValue, XboxController xboxController, Victor elevator) {
+	Elevator(DigitalInput limitUp, DigitalInput limitDown, XboxController xboxController, Victor elevator) {
 		
-		div = digitalInputValue;
+		limUp = limitUp;
+		limDown = limitDown;
 		xbox = xboxController;
 		elevate = elevator;
 		
 	}
 	
-	public void moveElevator() {
+	public void limOverride() {
 		
-		if (div.get()) {
+		if(override) {
 			
-			elevate.setSpeed(xbox.getY(GenericHID.Hand.kRight));
+			moveElevator();
+			
+		}
+		else {
+			
+			elevate.setSpeed(xbox.getY(GenericHID.Hand.kLeft));
+			
+		}
+	
+		
+		if(xbox.getStartButton() && xbox.getBackButton()) {
+			
+			override = false;
 			
 		}
 		
-		else {
+		else if(xbox.getBumper(GenericHID.Hand.kLeft) && xbox.getBumper(GenericHID.Hand.kRight)) {
 			
-			elevate.setSpeed(0);
+			override = true;
 			
 		}
 		
 	}
 	
+	public void moveElevator() {
+		
+		if (limDown.get()) { //Bottom limit switch pressed down
+			
+			if (xbox.getY(GenericHID.Hand.kLeft) < 0) { //Checks if Left Stick is pressed up
+				
+				elevate.setSpeed(-(xbox.getY(GenericHID.Hand.kLeft))); //Moves up
+				
+			}
+			
+			else {
+				
+				elevate.setSpeed(0); //No motion
+				
+			}
+			
+		}
+		
+		else if(limUp.get()) { //Top limit pressed down
+			
+			if(xbox.getY(GenericHID.Hand.kLeft) > 0) { //
+				
+				elevate.setSpeed(-(xbox.getY(GenericHID.Hand.kLeft)));
+				
+			}
+			
+			else {
+				
+				elevate.setSpeed(0);
+				
+			}
+			
+		}
+		else {
+			
+			elevate.setSpeed(.5*(xbox.getY(GenericHID.Hand.kLeft)));
+			
+		}
+	}
+	
+	public void simpleElevator() {
+		
+		elevate.setSpeed(xbox.getY(GenericHID.Hand.kRight));
+		
+	}
+	
 }
-
